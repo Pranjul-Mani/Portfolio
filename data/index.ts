8d2fd6858e887e89f2a45c6e19f25e5cb923cdd7
Pranjul Mani <pranjulmani010@gmail.com>
2025-03-28 13:09:23 +0530

    project change

--- data/index.ts
+++ data/index.ts
@@ -6,69 +6,6 @@ export const navItems = [
   { name: "Contact", link: "#contact" },
 ];
 
-export const gridItems = [
-  {
-    id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
-  },
-  {
-    id: 2,
-    title: "I'm very flexible with time zone communications",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-center",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 4,
-    title: "Tech enthusiast with a passion for development.",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
-  },
-
-  {
-    id: 5,
-    title: "Always tinkering with new technologies and libraries.",
-    description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
-  },
-  {
-    id: 6,
-    title: "Let's connect on Linkedin!",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
-  },
-];
 
 export const projects = [
   {
@@ -97,11 +34,11 @@ export const projects = [
   },
   {
     id: 4,
-    title: "TravelTales",
-    des: "Developed a social media application where you can login and create , like and delete posts.",
-    img: "/TT.png",
-    iconLists: ["/re.svg" , "/mongo.png","nodejs.png","/js.png"],
-    link: "https://github.com/Pranjul-Mani/TravelTales?tab=readme-ov-file",
+    title: "Simon Says",
+    des: "Developed a small game just for fun.",
+    img: "/ss.png",
+    iconLists: ["/html.png" , "/css-3.png","js.png"],
+    link: "https://pranjul-mani.github.io/Simon-game/",
   },
 ];
 
